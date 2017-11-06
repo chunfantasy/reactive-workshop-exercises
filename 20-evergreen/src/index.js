1f3932aeecde300e19c1f72cce3898a0488db6d2
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-11-06 14:33:14 +0000

    update

---
+++ 20-evergreen/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const HelloWorld = () => <div>Hello World!</div>;
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
