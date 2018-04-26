32903a75b49c14b2343065b3fa555eb5cad8190e
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-04-27 00:28:43 +0100

    update

---
+++ 41-rxjs-react/src/gameOfLife.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import classnames from 'classnames';
+import './Game.css';
+
+const cellKey = (row, column) => `${row}_${column}`;
+
+const isCellAlive = (currentState, row, column) => {
+  return !!currentState[cellKey(row, column)];
+}
+
+const toggle = (currentState, row, column) => {
+  const key = `${row}_${column}`;
+  return {...currentState, [key]: !currentState[key]};
+};
+
+const tick = currentState => {
+  const numberOfNeighbours = {};
+  for (let key in currentState) {
+    const [row, column] = key.split('_').map(p => parseInt(p, 10));
+    numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function ([offsetRow, offsetCol]) {
+      const neighbourKey = cellKey(row + offsetRow, column + offsetCol);
+      numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
+    });
+  }
+  const result = {};
+  for (let key in numberOfNeighbours) {
+    if (numberOfNeighbours[key] === 3 || (currentState[key] && numberOfNeighbours[key] === 2)) {
+      result[key] = true;
+    }
+  }
+  return result;
+};
+
+const GameOfLifePresentation = ({n, width, height, isAlive, onToggle, onTick}) => <div style={{width: n * width, height: n * height}}>
+  <div>
+    {
+      Array.from({length: n * n})
+        .map((value, index) => ({row: Math.floor(index / n), column: index % n}))
+        .map(({row, column}, index) => <div
+          key={index}
+          className={classnames({cell: true, alive: isAlive(row, column)})}
+          style={{width, height, top: 20 * row, left: 20 * column}}
+          onClick={() => onToggle({row, column})} />
+        )
+    }
+  </div>
+  <button onClick={onTick}>Tick</button>
+</div>;
+
+export {isCellAlive, toggle, tick, GameOfLifePresentation};
--- 41-rxjs-react/src/index.js
+++ 41-rxjs-react/src/index.js
@@ -3,21 +3,13 @@ import { render } from 'react-dom';
 import { Observable } from 'rxjs';
 import config from 'recompose/rxjsObservableConfig';
 import {setObservableConfig, componentFromStream, createEventHandler} from 'recompose';
-import './Game.css';
+import {isCellAlive, toggle, tick, GameOfLifePresentation} from './gameOfLife';
 
 setObservableConfig(config);
 
 const Hello = componentFromStream(
   props$ => props$.map(({name}) => <div>Hello {name}</div>)
 );
-
-// const Clock = componentFromStream(
-//   props$ => props$
-//     .map(({interval}) => Observable.interval(interval).startWith(0))
-//     .switch()
-//     .map(() => (new Date()).toLocaleString())
-//     .map(ts => <div>{ts}</div>)
-// );
 
 const Clock = componentFromStream(
   props$ => props$
@@ -38,50 +30,48 @@ const Counter = componentFromStream(
   }
 );
 
-const GameOfLife = componentFromStream(
-  props$ => {
-    const cellKey = (row, column) => `${row}_${column}`;
-    const reducer = (acc, action) => {
-      if (action.type === 'TOGGLE') {
-        const key = `${action.row}_${action.column}`;
-        return {...acc, [key]: !acc[key]};
-      } else if (action.type === 'TICK') {
-        const numberOfNeighbours = {};
-        for (let key in acc) {
-          const [row, column] = key.split('_').map(p => parseInt(p, 10));
-          numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
-          [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function ([offsetRow, offsetCol]) {
-            const neighbourKey = cellKey(row + offsetRow, column + offsetCol);
-            numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
-          });
-        }
-        const result = {};
-        for (let key in numberOfNeighbours) {
-          if (numberOfNeighbours[key] === 3 || (acc[key] && numberOfNeighbours[key] === 2)) {
-            result[key] = true;
-          }
-        }
-        return result;
-      }
-      return acc;
-    };
-    const {stream: onTick$, handler: onTick} = createEventHandler();
-    const {stream: onToggle$, handler: onToggle} = createEventHandler();
-    return Observable
-      .combineLatest(
-        props$,
-        Observable
-          .merge(
-            onTick$.mapTo({type: 'TICK'}),
-            onToggle$.map(position => ({type: 'TOGGLE', ...position}))
-          )
-          .startWith({})
-          .scan(reducer)
-      )
-      .map(([props, isAlive]) => ({...props, isAlive: (row, column) => isAlive[cellKey(row, column)], onToggle, onTick}))
-      .map(props => props.render(props));
-  }
-);
+const GameOfLifeContainer = componentFromStream(props$ => {
+  const reducer = (acc, action) => {
+    if (action.type === 'TOGGLE') {
+      return toggle(acc, action.row, action.column);
+    }
+    if (action.type === 'TICK') {
+      return tick(acc);
+    }
+    return acc;
+  };
+  const {stream: onToggle$, handler: onToggle} = createEventHandler();
+  const {stream: onTick$, handler: onTick} = createEventHandler();
+  return Observable
+    .combineLatest(
+      props$,
+      Observable
+        .merge(
+          onToggle$.map(position => ({type: 'TOGGLE', ...position})),
+          onTick$.mapTo({type: 'TICK'})
+        )
+        .startWith({})
+        .scan(reducer),
+      ({render}, isAlive) => render({isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick})
+    );
+});
+
+const GameOfLifeContainer2 = componentFromStream(props$ => {
+  const {stream: onToggle$, handler: onToggle} = createEventHandler();
+  const {stream: onTick$, handler: onTick} = createEventHandler();
+  return Observable
+    .combineLatest(
+      props$,
+      Observable
+        .merge(
+          onToggle$.map(({row, column}) => currentState => toggle(currentState, row, column)),
+          onTick$.mapTo(tick)
+        )
+        .startWith({})
+        .scan((currentState, action) => action(currentState)),
+      ({render}, isAlive) => render({isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick})
+    );
+});
 
 const App = () => <div>
   <h2>Hello World</h2>
@@ -94,23 +84,14 @@ const App = () => <div>
   <Counter />
 
   <h2>Game of Life</h2>
-  <GameOfLife n={10} width={20} height={20} render={
-    ({n, width, height, isAlive, onToggle, onTick}) => <div style={{width: n * width, height: n * height}}>
-      <div>
-        {
-          Array.from({length: n * n})
-            .map((value, index) => ({row: Math.floor(index / n), column: index % n}))
-            .map(({row, column}, index) => <div
-              key={index}
-              className={'cell' + (isAlive(row, column) ? ' alive' : '')}
-              style={{width, height, top: 20 * row, left: 20 * column}}
-              onClick={() => onToggle({row, column})}
-            />)
-        }
-      </div>
-      <button onClick={onTick}>Tick</button>
-    </div>
-  } />
+  <GameOfLifeContainer
+    render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />}
+  />
+
+  <h2>Game of Life V2</h2>
+  <GameOfLifeContainer2
+    render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />}
+  />
 </div>;
 
 render(<App />, document.getElementById('root'));
