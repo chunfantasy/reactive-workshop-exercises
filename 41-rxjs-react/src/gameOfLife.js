b20fcb2c875d04691f7963cc55d4c50b273cf781
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-06-04 17:34:43 +0100

    update

--- 41-rxjs-react/src/gameOfLife.js
+++ 41-rxjs-react/src/gameOfLife.js
@@ -4,14 +4,12 @@ import './Game.css';
 
 const cellKey = (row, column) => `${row}_${column}`;
 
-const isCellAlive = (currentState, row, column) => {
-  return !!currentState[cellKey(row, column)];
-}
+const isCellAlive = (currentState, row, column) => !!currentState[cellKey(row, column)];
 
 const toggle = (currentState, row, column) => {
   const key = `${row}_${column}`;
-  const {[key]: isAlive, ...result} = currentState;
-  return isAlive ? result : {...result, [key]: true};
+  const { [key]: isAlive, ...result } = currentState;
+  return isAlive ? result : { ...result, [key]: true };
 };
 
 const tick = currentState => {
@@ -19,7 +17,7 @@ const tick = currentState => {
   for (let key in currentState) {
     const [row, column] = key.split('_').map(p => parseInt(p, 10));
     numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
-    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function ([offsetRow, offsetCol]) {
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function([offsetRow, offsetCol]) {
       const neighbourKey = cellKey(row + offsetRow, column + offsetCol);
       numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
     });
@@ -33,20 +31,22 @@ const tick = currentState => {
   return result;
 };
 
-const GameOfLifePresentation = ({n, width, height, isAlive, onToggle, onTick}) => <div style={{width: n * width, height: n * height}}>
-  <div>
-    {
-      Array.from({length: n * n})
-        .map((value, index) => ({row: Math.floor(index / n), column: index % n}))
-        .map(({row, column}, index) => <div
-          key={index}
-          className={classnames({cell: true, alive: isAlive(row, column)})}
-          style={{width, height, top: 20 * row, left: 20 * column}}
-          onClick={() => onToggle({row, column})} />
-        )
-    }
+const GameOfLifePresentation = ({ n, width, height, isAlive, onToggle, onTick }) => (
+  <div style={{ width: n * width, height: n * height }}>
+    <div>
+      {Array.from({ length: n * n })
+        .map((value, index) => ({ row: Math.floor(index / n), column: index % n }))
+        .map(({ row, column }, index) => (
+          <div
+            key={index}
+            className={classnames({ cell: true, alive: isAlive(row, column) })}
+            style={{ width, height, top: 20 * row, left: 20 * column }}
+            onClick={() => onToggle({ row, column })}
+          />
+        ))}
+    </div>
+    <button onClick={onTick}>Tick</button>
   </div>
-  <button onClick={onTick}>Tick</button>
-</div>;
+);
 
-export {isCellAlive, toggle, tick, GameOfLifePresentation};
+export { isCellAlive, toggle, tick, GameOfLifePresentation };
--- 41-rxjs-react/src/index.js
+++ 41-rxjs-react/src/index.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Observable } from 'rxjs';
-import config from 'recompose/rxjsObservableConfig';
-import {setObservableConfig, componentFromStream, createEventHandler} from 'recompose';
-import {isCellAlive, toggle, tick, GameOfLifePresentation} from './gameOfLife';
+import { combineLatest, from, interval, merge } from 'rxjs';
+import { map, mapTo, scan, startWith, switchMap } from 'rxjs/operators';
+import { setObservableConfig, componentFromStream, createEventHandler } from 'recompose';
+import { isCellAlive, toggle, tick, GameOfLifePresentation } from './gameOfLife';
 
-setObservableConfig(config);
+setObservableConfig({ fromESObservable: from, toESObservable: stream => stream });
 
-const Hello = componentFromStream(
-  props$ => props$.map(({name}) => <div>Hello {name}</div>)
+const Hello = componentFromStream(props$ => props$.pipe(map(({ name }) => <div>Hello {name}</div>)));
+
+const Clock = componentFromStream(props$ =>
+  props$.pipe(
+    switchMap(props => interval(props.interval).pipe(startWith(0))),
+    map(() => new Date().toLocaleString()),
+    map(ts => <div>{ts}</div>)
+  )
 );
 
-const Clock = componentFromStream(
-  props$ => props$
-    .switchMap(({interval}) => Observable.interval(interval).startWith(0))
-    .map(() => (new Date()).toLocaleString())
-    .map(ts => <div>{ts}</div>)
-);
-
-const Counter = componentFromStream(
-  props$ => {
-    const {stream: onIncrement$, handler: onIncrement} = createEventHandler();
-    return Observable.combineLatest(props$, onIncrement$.scan((acc, step) => acc + step, 0).startWith(0))
-      .map(([props, value]) => ({value}))
-      .map(({value}) => <div>
+const Counter = componentFromStream(props$ => {
+  const { stream: onIncrement$, handler: onIncrement } = createEventHandler();
+  return combineLatest(
+    props$,
+    onIncrement$.pipe(
+      scan((acc, step) => acc + step, 0),
+      startWith(0)
+    )
+  ).pipe(
+    map(([props, value]) => ({ value })),
+    map(({ value }) => (
+      <div>
         {value}
         <button onClick={() => onIncrement(2)}>+</button>
-      </div>);
-  }
-);
+      </div>
+    ))
+  );
+});
 
 const GameOfLifeContainer = componentFromStream(props$ => {
   const reducer = (acc, action) => {
@@ -40,58 +46,54 @@ const GameOfLifeContainer = componentFromStream(props$ => {
     }
     return acc;
   };
-  const {stream: onToggle$, handler: onToggle} = createEventHandler();
-  const {stream: onTick$, handler: onTick} = createEventHandler();
-  return Observable
-    .combineLatest(
-      props$,
-      Observable
-        .merge(
-          onToggle$.map(position => ({type: 'TOGGLE', ...position})),
-          onTick$.mapTo({type: 'TICK'})
-        )
-        .startWith({})
-        .scan(reducer),
-      ({render}, isAlive) => render({isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick})
-    );
+  const { stream: onToggle$, handler: onToggle } = createEventHandler();
+  const { stream: onTick$, handler: onTick } = createEventHandler();
+  return combineLatest(
+    props$,
+    merge(
+      onToggle$.pipe(map(position => ({ type: 'TOGGLE', ...position }))),
+      onTick$.pipe(mapTo({ type: 'TICK' }))
+    ).pipe(
+      startWith({}),
+      scan(reducer)
+    ),
+    ({ render }, isAlive) => render({ isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick })
+  );
 });
 
 const GameOfLifeContainer2 = componentFromStream(props$ => {
-  const {stream: onToggle$, handler: onToggle} = createEventHandler();
-  const {stream: onTick$, handler: onTick} = createEventHandler();
-  return Observable
-    .combineLatest(
-      props$,
-      Observable
-        .merge(
-          onToggle$.map(({row, column}) => currentState => toggle(currentState, row, column)),
-          onTick$.mapTo(tick)
-        )
-        .startWith({})
-        .scan((currentState, action) => action(currentState)),
-      ({render}, isAlive) => render({isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick})
-    );
+  const { stream: onToggle$, handler: onToggle } = createEventHandler();
+  const { stream: onTick$, handler: onTick } = createEventHandler();
+  return combineLatest(
+    props$,
+    merge(
+      onToggle$.pipe(map(({ row, column }) => currentState => toggle(currentState, row, column))),
+      onTick$.pipe(mapTo(tick))
+    ).pipe(
+      startWith({}),
+      scan((currentState, action) => action(currentState))
+    ),
+    ({ render }, isAlive) => render({ isAlive: (row, column) => isCellAlive(isAlive, row, column), onToggle, onTick })
+  );
 });
 
-const App = () => <div>
-  <h2>Hello World</h2>
-  <Hello name="World" />
+const App = () => (
+  <div>
+    <h2>Hello World</h2>
+    <Hello name="World" />
 
-  <h2>Clock</h2>
-  <Clock interval={1000} />
+    <h2>Clock</h2>
+    <Clock interval={1000} />
 
-  <h2>Counter</h2>
-  <Counter />
+    <h2>Counter</h2>
+    <Counter />
 
-  <h2>Game of Life</h2>
-  <GameOfLifeContainer
-    render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />}
-  />
+    <h2>Game of Life</h2>
+    <GameOfLifeContainer render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />} />
 
-  <h2>Game of Life V2</h2>
-  <GameOfLifeContainer2
-    render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />}
-  />
-</div>;
+    <h2>Game of Life V2</h2>
+    <GameOfLifeContainer2 render={props => <GameOfLifePresentation n={10} width={20} height={20} {...props} />} />
+  </div>
+);
 
 render(<App />, document.getElementById('root'));
