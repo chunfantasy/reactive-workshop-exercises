afb4aa50e871f6498aec2a63b40c042ab8f45a4a
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-10-02 20:34:51 +0100

    update

---
+++ setupProxy.js
@@ -0,0 +1,37 @@
+const proxy = require('http-proxy-middleware');
+const config = {
+  '/api/motd': {
+    target: 'https://694y9v59lf.execute-api.eu-west-2.amazonaws.com',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      '^/api/motd': '/latest/motd'
+    }
+  },
+  '/api/gameOfLife': {
+    target: 'https://694y9v59lf.execute-api.eu-west-2.amazonaws.com',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      '^/api/gameOfLife': '/latest/gameOfLife'
+    }
+  },
+  '/api/todo': {
+    target: 'https://694y9v59lf.execute-api.eu-west-2.amazonaws.com',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      '^/api/todo': '/latest/todo'
+    }
+  },
+  '/api/ipify': {
+    target: 'https://api.ipify.org/',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      '^/api/ipify': ''
+    }
+  }
+};
+module.exports = app =>
+  Object.keys(config).forEach(path => app.use(proxy(path, config[path])));
