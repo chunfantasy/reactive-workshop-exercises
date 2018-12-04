1f7ce75c61fd4f18c7209fd713eb81b64ee48be5
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-12-04 10:13:53 +0100

    update

--- 05-events/src/ex-2/game-of-life.js
+++ 05-events/src/ex-2/game-of-life.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import './game-of-life.css';
 
 const GameOfLife = ({ n, width, height }) => (
   <div>
     <div style={{ width: n * width, height: n * height }}>
       {Array.from({ length: n * n })
-        .map((value, index) => ({ x: index % n, y: Math.floor(index / n) }))
-        .map(({ x, y }) => (
+        .map((_, index) => ({ row: index % n, column: Math.floor(index / n) }))
+        .map(({ row, column }) => (
           <div
-            className={'cell' + ((x + y) % 2 ? '' : ' alive')}
+            className={classnames({ cell: true, alive: (row + column) % 2 })}
             style={{
-              top: 20 * x,
-              left: 20 * y,
+              top: 20 * row,
+              left: 20 * column,
               width,
               height
             }}
