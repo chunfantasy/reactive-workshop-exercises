5c86c8987605d514764d2f79caa0805ae764e7ec
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-12-10 10:17:38 +0000

    update

--- 02-jsx/src/dont-peek/ex-1/index.js
+++ 02-jsx/src/dont-peek/ex-1/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MessageOfTheDay = () => {
-  const messages = [
-    'They don’t make bugs like Bunny anymore.',
-    'Talk is cheap. Show me the code.',
-    'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.',
-    'The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.',
-    'In theory, theory and practice are the same. In practice, they’re not.',
-    'Good design adds value faster than it adds cost.'
-  ];
-  return <div>{messages[Math.floor(messages.length * Math.random())]}</div>;
-};
+const messages = [
+  'They don’t make bugs like Bunny anymore.',
+  'Talk is cheap. Show me the code.',
+  'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.',
+  'The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.',
+  'In theory, theory and practice are the same. In practice, they’re not.',
+  'Good design adds value faster than it adds cost.'
+];
+
+const MessageOfTheDay = () => (
+  <div>{messages[Math.floor(messages.length * Math.random())]}</div>
+);
 
 render(<MessageOfTheDay />, document.getElementById('root-ex-1'));
