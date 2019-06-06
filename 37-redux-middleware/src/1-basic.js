fefacefcabc2245f3a59bbdada01cda7dab18a5b
Damjan Vujnovic <damjan@samuraiprinciple.com>
2019-06-06 18:26:21 +0100

    update

--- 37-redux-middleware/src/1-basic.js
+++ 37-redux-middleware/src/1-basic.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
-  <p>
-    {value}
-    <button onClick={onIncrement}>+</button>
-  </p>
-);
+const Counter = connect(
+  value => ({ value }),
+  dispatch => ({ onIncrement: () => dispatch({ type: 'INCREMENT' }) })
+)(({ value, onIncrement }) => <button onClick={onIncrement}>{value}</button>);
 
 const loggingMiddleware = store => next => action => {
   const previousState = store.getState();
   let result = next(action);
-  console.log(
-    `previousState: ${JSON.stringify(previousState)} action: ${JSON.stringify(
-      action
-    )} currentState: ${JSON.stringify(store.getState())}`
-  );
+  console.log(previousState, action, store.getState());
   return result;
 };
-
 const store = createStore(
   (state = 0, action) => (action.type === 'INCREMENT' ? state + 1 : state),
   applyMiddleware(loggingMiddleware)
 );
 
-const renderApp = () =>
-  render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    />,
-    document.getElementById('root')
-  );
-renderApp();
-store.subscribe(renderApp);
+render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root')
+);
