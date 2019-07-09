956f3bd071b5551f306ad74fac874ee35f659f33
Damjan Vujnovic <damjan@samuraiprinciple.com>
2019-07-09 15:05:41 +0100

    update

--- 05-events/src/7-dont-mutate-current-state.js
+++ 05-events/src/7-dont-mutate-current-state.js
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from 'react';
+import React, { memo, Component, PureComponent } from 'react';
 
 class Friends1 extends Component {
   render() {
@@ -26,6 +26,17 @@ class Friends2 extends PureComponent {
   }
 }
 
+const Friends3 = memo(({ friends }) => {
+  console.log('Friends3');
+  return (
+    <ul>
+      {friends.map(name => (
+        <li>{name}</li>
+      ))}
+    </ul>
+  );
+});
+
 class AddressBook extends Component {
   state = { value: 0, friends: [] };
   increment = () => this.setState(({ value }) => ({ value: value + 1 }));
@@ -47,6 +58,7 @@ class AddressBook extends Component {
         <button onClick={this.add2}>Add friend 2</button>
         <Friends1 friends={this.state.friends} />
         <Friends2 friends={this.state.friends} />
+        <Friends3 friends={this.state.friends} />
       </div>
     );
   }
